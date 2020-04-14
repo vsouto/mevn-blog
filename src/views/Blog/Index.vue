bb8540c8792ea892da5381f8c5b3993001d695ae
Victor Souto <souto.victor@gmail.com>
2020-04-14 14:32:07 -0300

    blog components

---
+++ src/views/Blog/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>Vue Blog</h2>
+    <p>Posts</p>
+    <ul>
+      <li v-for="post in posts"
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  computed: {
+    posts() {
+      return [];
+    }
+  }
+};
+</script>
---
+++ src/views/Blog/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>Vue Blog</h2>
+    <p>Posts</p>
+    <ul>
+      <li v-for="post in posts">
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Post',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
